0a7eab08b2234153f7c719e3d64f504fab37c818
alejandrodavidvela <alex.d.vela@gmail.com>
2021-05-05 23:37:25 -0500

    side drawer

--- App.js
+++ App.js
@@ -1,10 +1,47 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { createStore, combineReducers } from 'redux'
+import { Provider } from 'react-redux'
+import productsReducer from './store/reducers/products'
+import ShopNavigator from './navigation/ShopNavigator'
+import { AppLoading } from 'expo'
+import * as Font from 'expo-font'
+import cartReducer from './store/reducers/cart'
+import ordersReducer from './store/reducers/orders'
+
+
+
+const rootReducer = combineReducers({
+  products: productsReducer,
+  cart: cartReducer,
+  orders: ordersReducer
+})
+
+const store = createStore(rootReducer)
+
+const fetchFonts = () => {
+  return Font.loadAsync({
+    'roboto': require('./assets/fonts/Roboto-Regular.ttf'),
+    'roboto-bold': require('./assets/fonts/Roboto-Bold.ttf')
+  })
+}
 
 export default function App() {
+  const [fontLoaded, setFontLoaded] = useState(false)
+  
+  if(!fontLoaded){
+    return (
+      <AppLoading 
+        startAsync={fetchFonts} 
+        onFinish={() => {
+          setFontLoaded(true)
+        }}
+      />
+    )
+  }
+
   return (
-    <View>
-      <Text>A new app!</Text>
-    </View>
+    <Provider store={store}>
+      <ShopNavigator />
+    </Provider>
   );
 }
